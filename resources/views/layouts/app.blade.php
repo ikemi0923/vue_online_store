e37d4dadf05771cfd0051a56689c9a00a9313c35
ikemi <ikemi@ikeminoMacBook-Air.local>
2025-02-24 12:46:22 +0900

    Blade: APP_URL修正に伴い、CSSのURL生成を修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Vue Online Site')</title>
-    <link rel="stylesheet" href="{{ $prefix }}{{ parse_url(asset('css/style.css'), PHP_URL_PATH) }}">
+    <link rel="stylesheet" href="{{ $prefix }}/css/style.css">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 
